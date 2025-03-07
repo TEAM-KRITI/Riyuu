89522345eeb918a289ad63ed3f23ae7b5867076c
Clients11 <166890312+Clients11@users.noreply.github.com>
2025-03-08 00:19:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ VIDEO_DURATION_LIMIT = int(getenv("VIDEO_DURATION_LIMIT", 60*20)) # 20 minutes
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://yt.okflix.top/api') ## E.G https://yt.okflix.top/api/jADTdg-o8i0 Returns Download Info
 # Chat id of a group for logging bot's activities
 
-LOGGER_ID = int(getenv("LOGGER_ID", -1001964294961))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002389335496))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5111294407))
